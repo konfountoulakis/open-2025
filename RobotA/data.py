e7472dc13032b6294d01a2329b1aae7c2384ec05
TheodoraManika <93706717+TheodoraManika@users.noreply.github.com>
2025-02-15 10:42:18 +0200

    Update data.py

--- RobotA/data.py
+++ RobotA/data.py
@@ -1,7 +1,30 @@
 from microbit import *
 import radio
 
-def avg(values): pass
-def unit_transform(value): pass
-def encrypt(value): pass
-def collect_send(pin): pass
+def avg(values): 
+    total = sum(values)
+    count = len(values)
+    average = total / count
+    return average
+
+def change(value):
+    x = ((value*100)/1023)
+    return x 
+    
+def encrypt(sensor, value):
+    mes = sensor + str(value)
+    return mes
+    
+    
+def collect_send(sensor, pin):
+    values= []
+    for i in range(10):
+        values.append(pin.read_analog())
+        sleep(100)
+    average = change(avg(values))
+    mes= encrypt(sensor,average) 
+    radio.send(mes)
+
+    
+    change(value):
+    x = ((value))
