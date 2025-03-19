ba4214c57efedb29f3b642d8d768798889893853
TheodoraManika <93706717+TheodoraManika@users.noreply.github.com>
2025-03-19 20:18:28 +0200

    Update movements.py

--- RobotA/rover/movements.py
+++ RobotA/rover/movements.py
@@ -8,55 +8,45 @@ def stop(nz):
     nz.set_motors(1, 0)
     nz.set_motors(4, 0)
     
-def calc_distnce(distance):
-    return distance*80/100
 
 def straight(nz, speed): 
     nz.set_motors(1, speed)
     nz.set_motors(4, speed)
 
-def straight_distance(nz, speed, distance):
-    straight(nz, speed)
-    sleep(calc_distnce(distance))
-    stop(nz)
-
-def calc_deg(deg):
-    return deg*10/90
 
 def right(nz, speed):
-    nz.set_motors(1, speed / 2)
-    nz.set_motors(4, speed)   
+    nz.set_motors(1, 10)
+    nz.set_motors(4, speed * 2)   
 
-def right_deg(nz, speed, deg): 
-    nz.set_motors(1, speed / 2)
-    nz.set_motors(4, speed)
-    sleep(calc_deg(deg))
-    stop(nz)
 
-def left_deg(nz, speed, deg): 
-    nz.set_motors(1, speed)
-    nz.set_motors(4, speed / 2)
-    sleep(calc_deg(deg))
-    stop(nz)
+def left(nz, speed): 
+    nz.set_motors(1, speed * 2)
+    nz.set_motors(4, 10)
     
 def init_servo(nz): 
-    nz.set_servo(4, 90)
+    nz.set_servo(4, 0)
     
 def open(nz): 
     nz.set_servo(4, 90)
 def close(nz):
-    nz.set_servo(4, 180)
+    nz.set_servo(4, 0)
 
-def choose_movement(nz, ai, speed):
+def choose_movement(nz, ai):
     moves = ["straight", "right", "left"]
     move = random.choice(moves)
     if find_black_line(ai):
-        straight_distance(nz, -25, 50)
+        straight(nz, -25)
+        sleep(1000)
         move = random.choice(move[1:])
     if move == "straight":
-        straight_distance(nz, speed, random.randint(1, 10))
+        straight(nz, 25)
+        sleep(random.randint(1000, 5000))
+        stop(nz)
     elif move == "right":
-        right_deg(nz, speed, random.randint(1, 180))
+        right(nz, 50)
+        sleep(random.randint(1000, 5000))
+        stop(nz)
     elif move == "left":
-        left_deg(nz, speed, random.randint(1, 180))
-
+        left(nz, 50)
+        sleep(random.randint(1000, 5000))
+        stop(nz)
